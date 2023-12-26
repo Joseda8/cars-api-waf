5a7033694edb58f7ea112705d77e24d96768b65a
Ali Dasmeh <dasmeh@MacBook-Air.local>
2023-12-26 17:13:01 +0100

    sql injection ml init

--- waf/app/validators/sql_injection.py
+++ waf/app/validators/sql_injection.py
@@ -1,5 +1,31 @@
+import os
+import json
+from urllib.parse import unquote
+
 from app.validators.base import BaseValidator
 
+# these are needed for loading dataset and trained model files. 
+from flask import Blueprint
+main_bp = Blueprint("main", __name__)
+
+
+# ML model 
+import joblib
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+
+df_raw_text = pd.read_csv(main_bp.root_path+"/ml_files/sql_dataset.csv")
+
+# init vectorizer for tranfomation
+vectorizer = CountVectorizer(min_df=2, max_df=0.7, stop_words=stopwords.words('english'))
+# transform text to numbers (vectorizing)
+vectorizer.fit_transform(df_raw_text['Query'].values.astype('U')).toarray()
+# load saved model
+loaded_model = joblib.load(main_bp.root_path+"/ml_files/trained_model_with_94_percent_accuracy.sav")
 
 class SqlInjectionValidator(BaseValidator):
     def validate(self) -> bool:
@@ -15,10 +41,34 @@ class SqlInjectionValidator(BaseValidator):
         data = self.data
         cookies = self.cookies
         query_params = self.query_params
+        url = self.request_obj.url 
 
-        # Flag to indicate if the request is safe
         green_flag = True
 
-        # TODO: Make your magic here
+        if method in ("GET", "DELETE"):
+            for string_to_check in unquote(url).split('/'):
+                if not ml_model(string_to_check): 
+                    green_flag = False
+                    break
+
+        elif method in ("POST", "PUT"):
+            for key, value in (json.loads(data)[0]).items():
+                if type(value) == str:
+                    if not ml_model(value): 
+                        green_flag = False
+                        break
 
         return green_flag
+
+
+def ml_model(text_must_be_checked):
+    # transform text to numbers (vectorizing)
+    vectorized_req = vectorizer.transform([text_must_be_checked])
+
+    # prediction
+    result = loaded_model.predict(vectorized_req)
+    
+    if result[0] == 1:
+        return False # an injection detected
+    
+    return True
